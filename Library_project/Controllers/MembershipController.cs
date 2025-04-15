133b25311f22c18204a82e8dd655a08b34a7319a
Hafsana Begum <hafsas.ht@gmail.com>
2025-04-15 16:01:26 +0600

    Edit-Index
    Edit-Index

--- Library_project/Controllers/MembershipController.cs
+++ Library_project/Controllers/MembershipController.cs
@@ -1,4 +1,6 @@
-﻿using Library_project.Data;
+﻿using System.Diagnostics.Eventing.Reader;
+using Library_project.Data;
+using Library_project.DataModel;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +16,69 @@ namespace Library_project.Controllers
         public IActionResult MemberIndex()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Membersubmit (Member BookData)
+        {
+            if(BookData.MemberName !=null && BookData.MemberAddress !=null && BookData.MemberType !=null)
+            {
+                _context.Add(BookData);
+                _context.SaveChanges();
+
+            }
+            return RedirectToAction("MemberIndex");
+        }
+
+        public IActionResult MemberList()
+        {
+            var data = _context.Members.Where(x=>true).ToList() ;
+            return View(data);
+        }
+
+        public IActionResult MemberEdit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var Bookdata = _context.Members.FirstOrDefault(x => x.MemberId == id);
+            return View(Bookdata);
+        }
+
+        [HttpPost]
+        public IActionResult MemberEdit(Member Bookdata)
+        {
+            if(Bookdata.MemberName != null && Bookdata.MemberAddress != null && Bookdata.MemberType != null )
+            {
+
+            ////var data = _context.Members.Where(x => x.MemberId == Bookdata.MemberId).FirstOrDefault();
+            ////if(data != null)
+            //    {    
+            //    }
+                _context.Update(Bookdata);
+                    _context.SaveChanges();
+            }
+            return RedirectToAction("MemberList");
+        }
+
+        public IActionResult MemberDelete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Customer = _context.Members.FirstOrDefault(x => x.MemberId == id);
+
+            if (Customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(Customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("MemberList");
+
         }
     }
 }
