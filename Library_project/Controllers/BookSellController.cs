901dcf18f5ce4c868520f433b2e6b1ca3d2ddfbb
Hafsana Begum <hafsas.ht@gmail.com>
2025-04-29 16:05:20 +0600

    ajax-v2

--- Library_project/Controllers/BookSellController.cs
+++ Library_project/Controllers/BookSellController.cs
@@ -20,13 +20,17 @@ namespace Library_project.Controllers
        
          public IActionResult BookSells(Member Customer)
          {
+
+
+            var search = _context.Member.Where(x => x.MemberPhone == Customer.MemberPhone).ToList();
+
             if (Customer.MemberPhone !=null )
             {
                 _context.Member.Add(Customer);
                 _context.SaveChanges();
-                return Json(new { success = true, msg = "Data Saved!" });
+                return Json(new { success = true});
             }
-            return Json(new{success = false,msg = "faild!" });
+            return Json(new{success = false});
          }
    
     }
