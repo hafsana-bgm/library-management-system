5be6905bee3764679ac3312d24f4279d4c0dfeb3
Hafsana Begum <hafsas.ht@gmail.com>
2025-05-08 23:04:23 +0600

    auto_raw_in_script

--- Library_project/Controllers/BookSellController.cs
+++ Library_project/Controllers/BookSellController.cs
@@ -39,22 +39,6 @@ namespace Library_project.Controllers
             }
         }
 
-        [HttpPost]
-        //public IActionResult Account(FormCollection form)
-        //{
-        //    int quantity = int.Parse(form["Quantity"]);
-        //    double price = double.Parse(form["Price"]);
-        //    double discount = double.Parse(form["Discount"]);
-
-        //    double total = quantity* price;
-        //    double discountAmount = total * (double)(discount / 100.0);
-        //    double finalAmount = total - discountAmount;
-
-        //    return Json(new { Total = finalAmount});
-        //}
-
-
-
     }
 }
 
